fa36c3a266611ce7bf3cf9de57fdfca9203f1cd4
Guilherme Ribeiro Souza <guilhermerib2013@hotmail.com>
2024-03-09 09:46:42 -0300

    Atualização Ex9
    Exercicio 9 da lista de exercicios, programa pede o valor da hora/aula, quantidade de aulas e a procentagem de desconto do inss.

---
+++ Ex9.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main()
+{
+float hora_aula, desconto, salario_liquido;
+int aulas;
+
+printf("Digite o valor da hora_aula: ");
+scanf("%f", &hora_aula);
+
+printf("\nDigite a quantidade de aulas dadas: ");
+scanf("%d", &aulas);
+
+printf("\nDigite a porcentagem de desconto do inss: ");
+scanf("%f", &desconto);
+
+salario_liquido = (hora_aula*aulas) - (hora_aula*aulas)*(desconto/100);
+
+printf("\nO salario desse professor e: R$%.2f", salario_liquido);
+}
