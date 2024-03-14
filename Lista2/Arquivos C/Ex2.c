badf965d754340ce33e92dcd599c8df1602d11b6
Guilherme Ribeiro Souza <guilhermerib2013@hotmail.com>
2024-03-13 22:14:38 -0300

    Atualização Ex2
    Exercicio 2 - lista 2

---
+++ Lista2/Arquivos C/Ex2.c
@@ -0,0 +1,40 @@
+#include "stdio.h"
+
+void main(){
+int cd_inicial, aulas, nivel_professor;
+float salario;
+
+printf("Iniciar sistema - 1\nSair - 2\nDigite: ");
+scanf("%d", &cd_inicial);
+
+while(cd_inicial==1){
+
+    do{
+        printf("\n\nDigite o nivel do professor: ");
+        scanf("%d", &nivel_professor);
+    }while(nivel_professor!=1 && nivel_professor!=2 && nivel_professor!=3);
+
+
+    do{
+        printf("Digite a quantidade de aulas dadas: ");
+        scanf("%d", &aulas);
+    }while(aulas<0);
+
+    switch(nivel_professor){
+        case 1:
+            salario = 55*aulas;
+        break;
+        case 2:
+            salario = 67*aulas;
+        break;
+        case 3:
+            salario = 78*aulas;
+        break;
+    }
+
+    printf("O salario desse professor e %.2f", salario);
+
+    printf("\n\nIniciar novamente - 1\nSair - 2\nDigite:");
+    scanf("%d", &cd_inicial);
+}
+}
